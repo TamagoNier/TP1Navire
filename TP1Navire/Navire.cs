ca15f9bef8f890663a9447bb24ce21efbf889947
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2023-02-25 10:14:22 +0100

    creation depot git et premier commit

---
+++ TP1Navire/Navire.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1Navire
+{
+    class Navire
+    {
+        private string imo;
+        private string nom;
+        private string libelleFret;
+        private int qteFretMaxi;
+
+        public Navire(string imo, string nom)
+        {
+            this.imo = imo;
+            this.nom = nom;
+            this.libelleFret = "Indefini";
+            this.qteFretMaxi = 0;
+        }
+
+        public Navire(string imo, string nom, string libelleFret, int qteFretMaxi)
+        {
+            this.imo = imo;
+            this.nom = nom;
+            this.libelleFret = libelleFret;
+            this.qteFretMaxi = qteFretMaxi;
+        }
+
+        public string Imo { get => imo; }
+        public string Nom { get => nom; set => nom = value; }
+        public string LibelleFret { get => libelleFret; set => libelleFret = value; }
+        public int QteFretMaxi { get => qteFretMaxi; }
+    }
+}
---
+++ TP1Navire/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TP1Navire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TesterInstanciations();
+
+            Console.ReadKey();
+        }
+
+        private static void TesterInstanciations()
+        {
+            //Declaration de l'objet unNavire de la classe Navire
+            Navire unNavire;
+
+            //Instantiation de l'objet
+            unNavire = new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827);
+
+            //Declaration et instantiation de d'un autre objet de la classe Navire
+            Navire unAutreNavire = new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500);
+
+            //??
+            unAutreNavire = new Navire("IMO8715871", "MSC PILAR");
+
+            Console.WriteLine("Fin de la Methode");
+        }
+    }
+}
