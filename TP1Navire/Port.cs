31281b09089c1f37eb55933670e4b18874099074
Egor Gututui <102786178+TamagoNier@users.noreply.github.com>
2023-02-25 19:15:59 +0100

    fin partielle de la classe port 2

---
+++ TP1Navire/Port.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP1Navire
+{
+    class Port
+    {
+        private string nom;
+        private int nbNaviresMax;
+        private List<Navire> navires;
+
+        public Port(string nom)
+        {
+            this.nom = nom;
+            this.nbNaviresMax = 5;
+        }
+
+        public string Nom { get => nom; }
+        public int NbNaviresMax { get => nbNaviresMax; set => nbNaviresMax = value; }
+
+        public void EnregistrerArrive(Navire navire)
+        {
+            if(this.navires.Count == this.NbNaviresMax)
+            {
+                throw new Exception("Ajout Impossible, le port est complet");
+            }
+            else
+            {
+                this.navires.Add(navire);
+            }
+        }
+
+        public void EnregistrerDepart(string imo)
+        {
+            if (this.EstPresent(imo))
+            {
+                this.navires.RemoveAt(RecupPosition(imo));
+            }
+            else
+            {
+                throw new Exception("Le navire n'est pas present dans le port");
+            }
+            
+        }
+
+        public bool EstPresent(string imo)
+        {
+            for(int i = 0; i > this.navires.Count; i++)
+            {
+                if(this.navires[i].Imo == imo)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private int RecupPosition(string imo)
+        {
+            if (this.EstPresent(imo))
+            {
+                for (int i = 0; i > this.navires.Count - 1; i++)
+                {
+                    if (this.navires[i].Imo == imo)
+                    {
+                        int retour = i;
+                        return retour;
+                    }
+                }
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        private int RecupPosition(Navire navire)
+        {
+            if (this.navires.Contains(navire)){
+                int retour = this.navires.BinarySearch(navire);
+                return retour;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
--- TP1Navire/Program.cs
+++ TP1Navire/Program.cs
@@ -6,26 +6,52 @@ namespace TP1Navire
     {
         static void Main(string[] args)
         {
-            TesterInstanciations();
+            try {
+                //TesterInstanciations();
+                Navire depardieux = new Navire("IMO1564879", "SS Depardieux");
+                Navire astier = new Navire("IMO1564880", "SS Astier");
+                Navire chabat = new Navire("IMO1564881", "SS Chabat");
 
-            Console.ReadKey();
+                Port laSalle = new Port("Port LaSalle");
+                laSalle.EnregistrerArrive(astier);
+                laSalle.EnregistrerArrive(depardieux);
+                laSalle.EnregistrerArrive(chabat);
+
+                Console.ReadKey();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        private static void TesterInstanciations()
+        public static void TesterInstanciations()
         {
             //Declaration de l'objet unNavire de la classe Navire
             Navire unNavire;
 
             //Instantiation de l'objet
             unNavire = new Navire("IMO9427639", "Copper Spirit", "Hydrocarbures", 156827);
+            Affiche(unNavire);
 
             //Declaration et instantiation de d'un autre objet de la classe Navire
             Navire unAutreNavire = new Navire("IMO9839272", "MSC Isabella", "Porte-conteneurs", 197500);
+            Affiche(unAutreNavire);
 
             //??
             unAutreNavire = new Navire("IMO8715871", "MSC PILAR");
+            Affiche(unAutreNavire);
 
             Console.WriteLine("Fin de la Methode");
+        }
+
+        public static void Affiche(Navire navire)
+        {
+            Console.WriteLine("Identification : " + navire.Imo);
+            Console.WriteLine("Nom : " + navire.Nom);
+            Console.WriteLine("Type de frêt : " + navire.LibelleFret);
+            Console.WriteLine("Quantité de frêt : " + navire.QteFretMaxi);
+            Console.WriteLine("------------------------------");
         }
     }
 }
